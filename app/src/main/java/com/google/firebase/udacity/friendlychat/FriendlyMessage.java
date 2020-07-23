088ac84a7b212ecc20af8b588f5e93938922cfb1
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2020-07-23 11:25:19 +0000

    add setters and getters for id and imageUrl

--- app/src/main/java/com/google/firebase/udacity/friendlychat/FriendlyMessage.java
+++ app/src/main/java/com/google/firebase/udacity/friendlychat/FriendlyMessage.java
@@ -62,4 +62,20 @@ public class FriendlyMessage {
     public void setPhotoUrl(String photoUrl) {
         this.photoUrl = photoUrl;
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 }
